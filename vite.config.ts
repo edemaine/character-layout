874435d7033346b41e8bf6b5fa8ccafaf283a83b
Erik Demaine <edemaine@mit.edu>
2023-07-20 12:14:41 -0400

    Basic simplification of assemble code
    * linear, no circles
    * single row

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, PluginOption } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import civetPlugin from 'vite-plugin-civet'
+
+export default defineConfig({
+  base: "./",
+  plugins: [
+    solidPlugin(),
+    civetPlugin({
+      stripTypes: false,
+      outputExtension: 'tsx',
+      outputTransformerPlugin: 'solid',
+    }) as PluginOption,
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+})
